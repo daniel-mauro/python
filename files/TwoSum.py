c71788bc9ecdbd897e042d72d6cb3cf4074e98a6
daniel-mauro <mauromortgage@gmail.com>
2021-01-28 22:46:37 -0500

    added a file for you

---
+++ files/TwoSum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        i = 0
+        
+        size = len(nums)
+        
+        while i < size - 1:
+            j = i + 1
+            while j < size:
+                if (nums[i] + nums[j] == target):
+                    sol = [i,j]
+                    return sol
+                j += 1
+            i += 1
+  
